5fe47639e87f2b628380aac7fb01351a11fe8e3a
Emma Mellgren <mellgrenemma0@gmail.com>
2024-10-29 18:37:21 +0100

    updated UI

--- app/components/accommodationDetails.tsx
+++ app/components/accommodationDetails.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image';
 import EmblaCarousel from '@/components/carousel';
 import { EmblaOptionsType } from 'embla-carousel';
 import iconMappingToNode from './ui/propertiesIcons';
+import Navigation from './navigation';
+import smilingMan from '../lib/images/smiling-man.jpg';
 
 type AccommodationDetailsProps = {
     accommodation: Accommodation | null;
@@ -42,7 +44,10 @@ const AccommodationDetails = ({ accommodation }: AccommodationDetailsProps) => {
                     options={OPTIONS}
                 />
             </div>
-            <div className='hidden md:grid mx-auto gap-2 md:grid-cols-2 md:w-[800px]'>
+            <span className='hidden md:block'>
+                <Navigation />
+            </span>
+            <div className='mt-6 hidden md:grid mx-auto gap-2 md:grid-cols-2 md:w-[800px]'>
                 <div className='h-[400px]'>
                     <Image
                         src={accommodation.images[0]}
@@ -70,23 +75,49 @@ const AccommodationDetails = ({ accommodation }: AccommodationDetailsProps) => {
                 </div>
             </div>
             <div className='flex flex-col gap-2 px-8 py-4 md:px-16'>
-                <p className='title'>{accommodation.title}</p>
-                <div className='flex justify-between'>
-                    <p>Host: Catherine</p>
-                    <p>Price: {accommodation.price} SEK / night</p>
-                    <p>Location: {accommodation.location}</p>
+                <div className='mb-4'>
+                    <p className='title'>{accommodation.title}</p>
+                    <div className='flex justify-between'>
+                        <span className='flex items-center gap-1'>
+                            <p className='bold'>Location:</p>
+                            <p>{accommodation.location}</p>
+                        </span>
+                        <span className='flex items-center gap-1'>
+                            <p className='bold'>Price:</p>
+                            <p>{accommodation.price} SEK / night</p>
+                        </span>
+                    </div>
                 </div>
-                <div className='flex flex-col gap-2'>
+                <div className='flex justify-between'>
+                    <span className='flex items-center gap-1'>
+                        <Image
+                            src={smilingMan}
+                            alt='Host'
+                            width={40}
+                            height={40}
+                            className='rounded-full object-cover aspect-square'
+                        />
+                        <p className='bold pl-2'>Hosted by</p>
+                        <p>Alex</p>
+                    </span>
+                </div>
+                <div className='flex flex-col gap-4 my-6'>
                     <p className='caption'>This accommodation offers</p>
-                    <span>
+                    <span className='flex gap-4 items-center'>
                         {accommodation.properties?.map((property, index) => (
-                            <span key={index}>
+                            <span
+                                key={index}
+                                className='flex flex-col text-xs items-center gap-1'>
                                 {iconMappingToNode[property as string]}
+                                {property}
                             </span>
                         ))}
                     </span>
                 </div>
-                <p>{accommodation.description}</p>
+                <div className='flex flex-col gap-2'>
+                    <p className='bold'>About this space</p>
+                    <p>{accommodation.description}</p>
+                </div>
             </div>
         </>
     );
